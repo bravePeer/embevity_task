039d2bf80586431a280f008dbf2b93b91118df3f
Bartek <bartek.gojowczyk01@gmail.com>
2025-05-19 00:42:00 +0200

    Update communication with sensor

--- MainApp/lib/sensor/include/sensor.hpp
+++ MainApp/lib/sensor/include/sensor.hpp
@@ -4,56 +4,24 @@
 #include "client.hpp"
 #include "../../Simulator/include/memory_map.hpp"
 
-void readSensorReg(Client* c, MemoryAddresses addr);
+/// @brief Read register value from sensor if error occure throw exception
+/// @param c Pointer to client to connect to simulator
+/// @param addr Address of register
+/// @return Register Value
+char readSensorReg(Client* c, MemoryAddresses addr);
 
-/// @brief Sets register responsible for power up device
-/// @param c pointer to client
-/// @param val if 1 device is power up else is power down
-void setPowerDown(Client* c, char val)
-{
-    char buffer[2];
-    buffer[0] = static_cast<char>(MemoryAddresses::PowerDown);
-    buffer[1] = val;
-    c->writeReg(SLAVE_ADDRESS, buffer, 2);
-}
+/// @brief Sets register responsible for power up device if error occure throw exception
+/// @param c Pointer to client to connect to simulator
+/// @param val If 1 device is power up else if 0 is power down
+void setPowerDown(Client* c, char val);
 
-void setReset(Client* c, char val)
-{
-    char buffer[2];
-    buffer[0] = static_cast<char>(MemoryAddresses::Reset);
-    buffer[1] = val;
-    c->writeReg(SLAVE_ADDRESS, buffer, 2);
-}
+/// @brief Sets register responsible for reset device if error occure throw exception
+/// @param c Pointer to client to connect to simulator
+/// @param val If 1 device is ready to work else if 0 is in reset mode
+void setReset(Client* c, char val);
 
-/// @brief 
-/// @param c 
-/// @param pressure Pa
-/// @param temperature *C
-void readPressureTemperature(Client* c, int* pressure, float* temperature)
-{
-    char buffer[5];
-    int len = 5;
-    buffer[0] = static_cast<char>(MemoryAddresses::PressureMSB);
-    c->writeReg(SLAVE_ADDRESS, buffer, 1);
-    c->readReg(SLAVE_ADDRESS, buffer, &len);
-
-    if(len != 5)
-        throw "ERROR: received wrong number of bytes";
-
-    int tmpPress = buffer[0] & 0xff;
-    tmpPress <<= 8;
-    tmpPress |=  buffer[1] & 0xff;
-    tmpPress <<= 6;
-    tmpPress |=  buffer[2] & 0x3f;
-    
-
-    float tmpPressF = (static_cast<float>(tmpPress) / 2048.f) * 100.f;
-    *pressure = static_cast<int>(std::round(tmpPressF));
-
-    unsigned int tmpTemp = buffer[3] & 0xff;
-    tmpTemp <<= 8;
-    tmpTemp |= buffer[4] & 0xff;
-    
-    *temperature = static_cast<float>(tmpTemp) / 32.f;
-}
-
+/// @brief Reads pressure and temperature from sensor if error occure throw exception
+/// @param c Pointer to client to connect to simulator
+/// @param pressure Pointer where to store pressure in Pa
+/// @param temperature Pointer where to store temperature in *C
+void readPressureTemperature(Client* c, int* pressure, float* temperature);
--- MainApp/lib/sensor/src/sensor.cpp
+++ MainApp/lib/sensor/src/sensor.cpp
@@ -1,6 +1,6 @@
 #include "sensor.hpp"
 
-void readSensorReg(Client* c, MemoryAddresses addr)
+char readSensorReg(Client* c, MemoryAddresses addr)
 {
     char buffer[1];
     buffer[0] = static_cast<char>(addr);
@@ -8,7 +8,49 @@ void readSensorReg(Client* c, MemoryAddresses addr)
     c->writeReg(SLAVE_ADDRESS, buffer, 1); // Set read reg
     c->readReg(SLAVE_ADDRESS, buffer, &len);
 
-    std::cout<<"Reg val = ";
-    printCharArray(buffer, len);
-    std::cout << std::endl;
+    return buffer[0];
+}
+
+void setPowerDown(Client* c, char val)
+{
+    char buffer[2];
+    buffer[0] = static_cast<char>(MemoryAddresses::PowerDown);
+    buffer[1] = val;
+    c->writeReg(SLAVE_ADDRESS, buffer, 2);
+}
+
+void setReset(Client* c, char val)
+{
+    char buffer[2];
+    buffer[0] = static_cast<char>(MemoryAddresses::Reset);
+    buffer[1] = val;
+    c->writeReg(SLAVE_ADDRESS, buffer, 2);
+}
+
+void readPressureTemperature(Client* c, int* pressure, float* temperature)
+{
+    char buffer[5];
+    int len = 5;
+    buffer[0] = static_cast<char>(MemoryAddresses::PressureMSB);
+    c->writeReg(SLAVE_ADDRESS, buffer, 1);
+    c->readReg(SLAVE_ADDRESS, buffer, &len);
+
+    if(len != 5)
+        throw "ERROR: received wrong number of bytes";
+
+    int tmpPress = buffer[0] & 0xff;
+    tmpPress <<= 8;
+    tmpPress |=  buffer[1] & 0xff;
+    tmpPress <<= 6;
+    tmpPress |=  buffer[2] & 0x3f;
+    
+
+    float tmpPressF = (static_cast<float>(tmpPress) / 2048.f) * 100.f;
+    *pressure = static_cast<int>(std::round(tmpPressF));
+
+    unsigned int tmpTemp = buffer[3] & 0xff;
+    tmpTemp <<= 8;
+    tmpTemp |= buffer[4] & 0xff;
+    
+    *temperature = static_cast<float>(tmpTemp) / 32.f;
 }
